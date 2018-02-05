ad0f5c9ec3efb2a012289e76041d4f7e76241180
Henry <hendrisetiawardana@gmail.com>
2018-02-05 23:28:55 +0800

    FInal

---
+++ Day3ExerciseSectionF/Program 2.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day3ExerciseSectionF
+{
+    class Program2
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Please enter a phrase: ");
+            string phr = Console.ReadLine();
+            
+                for(int i = 0; i < phr.Length/2; i++)
+                {
+                    if (phr.Substring(i, 1) != phr.Substring(phr.Length - i - 1, 1))
+                    {
+                        Console.WriteLine("{0} is Not a Palindrome", phr);
+                        return;
+                    }
+                }
+            
+            Console.WriteLine("{0} is a Palindrome", phr);
+        }
+    }
+}
--- Day3ExerciseSectionF/Program.cs
+++ Day3ExerciseSectionF/Program.cs
@@ -10,6 +10,48 @@ namespace Day3ExerciseSectionF
     {
         static void Main(string[] args)
         {
+            Console.Write("Please enter a phrase: ");
+            string phr = Console.ReadLine();
+            phr = phr.ToUpper();
+            string s;
+            int a=0, e = 0, i = 0, o = 0, u = 0;
+
+            for (int j=0; j<phr.Length; j++)
+            {
+                s = phr.Substring(j, 1);
+
+                switch (s)
+                {
+                    case "A":
+                        a++;
+                        break;
+                    case "E":
+                        e++;
+                        break;
+                    case "I":
+                        i++;
+                        break;
+                    case "O":
+                        o++;
+                        break;
+                    case "U":
+                        u++;
+                        break;
+                    default:
+                        break;
+                }
+
+            }
+
+            int total = a + e + i + o + u;
+
+            Console.WriteLine("Total vowels: {0}", total);
+            Console.WriteLine("A: {0}", a);
+            Console.WriteLine("E: {0}", e);
+            Console.WriteLine("I: {0}", i);
+            Console.WriteLine("O: {0}", o);
+            Console.WriteLine("U: {0}", u);
+
         }
     }
 }
